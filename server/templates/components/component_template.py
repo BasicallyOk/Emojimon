8119416623d790a241e3ae8188d0002306ce21af
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-09-09 15:45:08 -0400

    Added component class template to make ur lives easier

---
+++ server/templates/components/component_template.py
@@ -0,0 +1,54 @@
+"""
+This file is a template for what you should start with and should do.
+Provide a detailed description of the file/component here.
+"""
+
+# import statements go here
+
+__author__ = "" # Insert your name here if you're writing the initial code
+__maintainer__ = "" # Insert your name here if you're fixing bugs or make improvements
+
+class Template:
+    """Brief description here"""
+
+    # Define class attributes 
+    param_1 = 0 # Give short descriptions 
+    param_2 = [] # Description everywhere, I want to see more green text
+
+    def __init__(param1:int, param2:str='example'):
+        """
+        Constructor for Template.
+
+        Keyword arguments:
+            param1 (int) -- give description of what this is, as well as what it should include
+            param2 (str) -- whether we should create the trainer in db. Default to True if id is None.
+
+        Returns:
+            If anything is returned, describe it here
+        """
+        pass
+
+    
+    def attr_func(self, param1: int):
+        """
+        Give short description of what the function does, plus a short summary of how.
+        This is just a constructor so we can keep it short.
+        Keyword arguments are of EXTREME IMPORTANCE. Code readability starts here.
+
+        Keyword arguments:
+            param1 (int) -- give description of what this is, as well as what it should include
+
+        Returns:
+            If anything is returned, describe it here
+        """
+        pass
+
+
+def main(): 
+    """Main driver code for testing"""
+    pass
+
+
+if __name__ == "__main__":
+    """Runs when specifically called from command line"""
+    main()
