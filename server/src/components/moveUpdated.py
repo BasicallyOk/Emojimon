f3f9b9cf434862c29780cab24c3e184d86aa72fd
ComeOnConMan <killingbeck14@gmail.com>
2022-10-02 14:47:17 -0400

    Added the method of determining type effectiveness

--- server/src/components/moveUpdated.py
+++ server/src/components/moveUpdated.py
@@ -44,15 +44,62 @@ class Move:
     
     #A function that returns True if the move is being used is the same type as the moves user
     def determineSameTypeAttackBonus(self):
-        tempEmoji = getMoveOwnerAsEmojimon()
+        #tempEmoji = getMoveOwnerAsEmojimon()
         #check if the emoji's type and getMoveType return the same type, if so, return true
+        pass
+    
+    #A function that determins the move type multiplier. If the moves type is effective aginst the opponent.
+    def determineTypeEffectivenessMultiplier(self, moveTarget):
+        pass
+    
+    #The actual function that goes through the chart looking for the desired type
+    def typeEffectivesChart(moveType, defenderType):
+        typeChart = [["Move Type >", "Normal", "Robot", "Ninja", "Fire", "Water", "Dinosaur", "Earth", "Sound", "Wind", "Darkness", "Light", "Plasma", "Solar", "Lunar", "Meme", "Magic"],
+                     ["Normal",         "1",       "1",     "1",    "1",    "1",       "2",       "1",    "1",     "1",     "1",        "1",     "2",     "2",     "1",    "1",      "1"],
+                     ["Robot",          "0.5",     "1",     "1",  "0.5",    "2",       "1",       "1",  "0.5",     "1",     "1",        "1",     "2",     "2",   "0.5",    "1",      "3"],
+                     ["Ninja",          "0.5",     "1",     "2",    "1",    "1",       "1",       "1",    "1",     "1",   "0.5",        "3",     "1",     "1",     "1",    "2",      "1"],
+                     ["Fire",           "1",       "1",     "1",    "1",    "3",     "0.5",       "2",    "1",     "2",   "0.5",        "1",     "1",     "1",     "1",    "1",      "1"],
+                     ["Water",          "1",     "0.5",     "1", "0.25",    "1",       "1",       "1",    "1",     "1",     "1",        "1",     "2",     "1",     "3",    "1",      "1"],
+                     ["Dinosaur",     "0.5",       "1",     "1",    "2",    "1",       "3",       "1",    "1",     "1",     "2",        "1",     "1",   "0.5",     "2",    "1",      "2"],
+                     ["Earth",          "1",       "1",   "0.5",  "0.5",    "2",       "1",       "1",    "1",   "0.5",     "1",        "1",     "1",     "1",     "1",    "1",      "1"],
+                     ["Sound",          "1",     "0.5",     "1",    "1",    "1",       "1",       "2",    "1",     "2",     "2",        "1",     "1",     "1",     "1",    "3",      "2"],
+                     ["Wind",         "0.5",     "0.5",     "2",    "2",    "1",       "1",       "1",    "2",     "1",     "1",        "1",     "1",     "1",     "1",  "0.5",      "1"],
+                     ["Darkness",     "0.5",       "1",     "2",    "1",    "1",       "1",       "1",    "1",   "0.5",     "1",        "3",     "1",     "2",  "0.25",    "1",   "0.25"],
+                     ["Light",          "1",       "1",   "0.5",    "1",    "1",       "1",       "2",    "1",     "1",  "0.25",        "1",     "1",     "1",     "1",    "1",    "0.5"],
+                     ["Plasma",         "2",       "1",     "1",    "1", "0.25",     "0.5",       "1",    "1",     "1",     "1",     "0.25",     "1",     "3",     "1",  "0.5",      "1"],
+                     ["Solar",          "2",       "2",     "1",  "0.5",  "0.5",     "0.5",       "1",  "0.5",   "0.5",  "0.25",        "1",     "1",     "1",     "2",    "1",      "1"],
+                     ["Lunar",          "1",       "2",     "2",    "1",    "2",       "1",       "1",  "0.5",     "1",     "2",        "1",   "0.5",     "1",     "1",    "1",      "1"],
+                     ["Meme",         "0.5",       "3",   "0.5",    "1",    "1",       "1",     "0.5",    "3",     "1",     "1",        "1",     "1",     "1",     "1",    "3",      "1"],
+                     ["Magic",          "1",       "1",     "1",    "2",    "1",       "2",     "0.5",    "3",     "2",     "1",        "1",   "0.5",     "1",     "1",    "1",      "1"]]
         
-    
+        type1Point = 0
+        type2Point = 0
         
-    
-    
-    
+        for type1 in typeChart[0]:
+            if(moveType == type1):
+                break
+            else:
+                type1Point += 1
         
+        for x in range(16):
+            if(defenderType == typeChart[x][0]):
+                break
+            else:
+                type2Point += 1
+        
+        return typeChart[type2Point][type1Point]
+            
+     
+"""  Test Code
+def main():
+    print(Move.typeEffectivesChart("Magic", "Dinosaur"))
+    pass
+       
+    
+if __name__ == "__main__":
+    main()
+    
+"""  
     
     
     
