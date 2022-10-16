c78a1d99b3272d7bfa80dc8cba469fce3778f3a4
ComeOnConMan <killingbeck14@gmail.com>
2022-10-16 03:18:05 -0400

    UNG!

---
+++ server/src/components/effect_handle.py
@@ -0,0 +1,25 @@
+"""
+Effect handle manages the active effects on an emoji
+"""
+
+from components import emojimon, move, moveUpdated
+
+__author__ = "Connor Killingbeck" # Insert your name here if you're writing the initial code
+__maintainer__ = "Connor Killingbeck" # Insert your name here if you're fixing bugs or make improvements
+
+class Effect_Handle:
+    active_effects = []
+    
+    
+    
+    
+    
+
+class Effect:
+    effect_code = ""
+    
+    def __init__(self, passed_code):
+        self.effect_code = passed_code
+    
+    def purge_done_effects():
+        pass
